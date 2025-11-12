b74d80d64f48c2bcfa1dcbfd805013c496e4ef0e
saitenntaisei <saitenntaisei@gmail.com>
2025-11-12 16:06:37 +0900

    feat: modify keybind of oil, file manager

--- neovim/lua/plugins/oil.lua
+++ neovim/lua/plugins/oil.lua
@@ -5,7 +5,13 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			keymaps = {
+				["<C-j>"] = "actions.select", -- dive into the entry under cursor
+				["<C-k>"] = "actions.parent", -- jump to parent directory
+                ["<C-n>"] = "actions.refresh", -- refresh the tree
+			},
+		})
 		vim.keymap.set("n", "<C-n>", require("oil").open)
 	end,
 }
